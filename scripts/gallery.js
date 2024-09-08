d13b5ac01ff63ba3d82de01805147ef8f734ac22
Igor Manoryk <imanoryk6@gmail.com>
2024-09-08 19:45:00 +0200

    poprawa loga i wyglądu na telefonie

---
+++ scripts/gallery.js
@@ -0,0 +1,40 @@
+// todo: finish
+// const photosContainer = document.querySelector('[gallery]');
+
+// function main() {
+//     clearPhotosContainer();
+//     const photos = getPhotosToDisplay();
+//     displayPhotos(photos);
+// }
+
+// function clearPhotosContainer() {
+//     photosContainer.innerHTML = '';
+// }
+
+// function getPhotosToDisplay() {
+//     const photoNodes = [];
+//     for (let i = 0; i < 100; i++) {
+//         const photoLink = document.createElement('a');
+
+//         photoLink.classList.add('photo');
+
+//         photoLink.setAttribute('href', HREF!!!);
+//         photoLink.setAttribute('target', '_blank');
+
+//         const photoImage = document.createElement('img');
+//         photoImage.setAttribute('src', HREF!!!);
+//         photoImage.setAttribute('alt', 'zdjęcie');
+
+//         photoLink.appendChild(photoImage);
+//         photoNodes.push(photoLink);
+//     }
+//     return photoNodes;
+// }
+
+// function displayPhotos(photos) {
+//     for (const photo of photos) {
+//         photosContainer.appendChild(photo);
+//     }
+// }
+
+// main();
