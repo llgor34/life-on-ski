52c752da7e331d8f12bed8cb00f4e28c2953d181
Igor Manoryk <imanoryk6@gmail.com>
2024-11-11 19:31:44 +0100

    poprawa navbara

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,16 @@
 const navButton = document.querySelector('.mobile-button');
 const navbar = document.querySelector('.navbar');
+const navLinks = navbar.querySelectorAll('a');
 
 navButton.addEventListener('click', (e) => {
     e.preventDefault();
     navbar.classList.toggle('active');
 });
+
+navLinks.forEach((navLink) =>
+    navLink.addEventListener('click', (e) => {
+        if (window.innerWidth < 1000 && navbar.classList.contains('active')) {
+            navbar.classList.remove('active');
+        }
+    })
+);
