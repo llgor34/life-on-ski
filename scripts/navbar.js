7f1d2f5f1066d92d4f79b13c958c9b4e5edaf5fe
Igor Manoryk <imanoryk6@gmail.com>
2024-11-20 17:54:02 +0100

    poprawa scrollowania z navbara

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -9,8 +9,40 @@ navButton.addEventListener('click', (e) => {
 
 navLinks.forEach((navLink) =>
     navLink.addEventListener('click', (e) => {
-        if (window.innerWidth < 1000 && navbar.classList.contains('active')) {
-            navbar.classList.remove('active');
+        const element = e.target;
+
+        if (shouldScrollToElement(element)) {
+            e.preventDefault();
+
+            const scrollElementSelector = getElementSelector(element);
+            closeNavbarIfNecessary().then(() => scrollToElementBySelector(scrollElementSelector));
         }
     })
 );
+
+function shouldScrollToElement(element) {
+    return element.hasAttribute('scrollToElement');
+}
+
+function getElementSelector(element) {
+    return `#${element.href.split('#')[1]}`;
+}
+
+function closeNavbarIfNecessary() {
+    return new Promise((resolve, reject) => {
+        if (navbar.classList.contains('active')) {
+            navbar.classList.remove('active');
+            navbar.addEventListener('transitionend', resolve);
+        } else {
+            resolve();
+        }
+    });
+}
+
+function scrollToElementBySelector(selector) {
+    const element = document.querySelector(selector);
+    const offset = navbar.getBoundingClientRect().height;
+    const top = element.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({ top, behavior: 'smooth' });
+}
